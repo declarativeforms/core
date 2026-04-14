5cb2841f098e6ac494f080345ece20a5b052ddad
Barend Erasmus <hirebarend@gmail.com>
2026-04-14 14:15:26 +0200

    fixes

--- packages/api/src/routes/studio-forms-get.ts
+++ packages/api/src/routes/studio-forms-get.ts
@@ -2,9 +2,10 @@ import type { FastifyReply, FastifyRequest, RouteOptions } from 'fastify';
 import { getContainer } from '../core';
 
 export const STUDIO_FORMS_GET: RouteOptions<any, any, any, any> = {
-  handler: async (_request: FastifyRequest, reply: FastifyReply) => {
+  handler: async (request: FastifyRequest, reply: FastifyReply) => {
     const { studioFormService } = await getContainer();
-    const forms = await studioFormService.list();
+    const email = request.studioUser?.username ?? '';
+    const forms = await studioFormService.list(email);
     reply.status(200).send(forms);
   },
   method: 'GET',
